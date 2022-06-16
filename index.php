b9a8e41590fcaecc9ed635fdb5c195335e28e57b
Apple197 <73128879+Apple197@users.noreply.github.com>
2022-06-17 00:07:50 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+<html>
+<head>
+<style>
+*{
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+}
+.container{
+height: 100vh;
+width: 100vw;
+Background: white;
+display: flex;
+justify-content: center;
+align-items: center;
+}
+.container div{
+background: #fff;
+box-shadow: 0 0 30px grey;
+padding: 20px;
+border-radius: 10px;
+}
+p{
+font-size: 25px;
+font-weight: 600;
+text-align: center;
+}
+</style>
+</head>
+<body>
+ <div class="container">
+    <div>
+       <P>Work in progress</p>
+    </div>
+ <div>
+</body>
+</html>
+?>
